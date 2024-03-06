dc78ccfe5cdeb22d7f4064a52b7ac7a0f7c754f6
Ferdinando <fdinocera@libero.it>
2024-03-06 16:53:39 +0100

    esercizio

--- Unit2/Sett3/Giorno3/assets/js/script.js
+++ Unit2/Sett3/Giorno3/assets/js/script.js
@@ -1,84 +1,115 @@
-// var Persona = /** @class */ (function () {
-//     function Persona() {
-//     }
-//     Persona.motto = 'viva la vita!';
-//     return Persona;
-// }());
-// var pers = new Persona;
-// console.log(pers.nome);
-
-
-
-// // Questa funzione carica qualcosa nell'HTML della pagina promise.html, a seguito di una richiesta HTTP e di una promise
-// function showPage(page) {
-//     document.getElementById('pagina').innerHTML = page;
-// }
-
-// // Questa funzione è la promise che cerca di recuperare il file da includere
-// const promise = new Promise(function (resolve, reject) {
-//     let request = new XMLHttpRequest(); // Richiesta HTTP...
-//     request.open('GET', 'include.html'); // ... di tipo GET, per cercare la risorsa indicata dopo la virgola (include.html)
-//     request.onload = function () {
-//         // Caricamento della richiesta
-//         if (request.status == 200) {
-//             // Una richiesta HTTP risponde con uno status code (lezione del 5/3); se lo status code è 200 vuol dire che la richiesta è andata a buon fine, quindi la promise è risolta (resolve)...
-//             resolve(request.response); // ... e legge la risposta della richiesta, cioè la risorsa trovata...
-//         } else {
-//             reject('<h3>FILE NON TROVATO!</h3>'); // ... altrimenti la promise NON È risolta (reject), e gestisce l'errore, in questo caso indicando nell'HTML l'errore
-//         }
-//     };
-//     request.send(); // La richiesta così definita parte verso il server
-// });
-
-// promise.then(
-//     // La promise eseguita con il metodo then gestisce i due possibili casi:
-//     function (value) {
-//         // 1. se la promise è risolta, il valore del resolve (riga 14) viene passato alla funzione showPage
-//         showPage(value);
-//     },
-//     function (error) {
-//         // 2.. se la promise non è risolta, il valore del reject (riga 16) viene passato alla funzione showPage
-//         showPage(error);
-//     }
-// );
-
-// const p = new Promise((resolve, reject) => {
-
-//     console.log(resolve);
-
-//     console.log(reject);
-
-// });
-
-// p.then(
-//     function a()
-//     console.log('then 1')
-// )
-
 let endpoint = 'https://striveschool-api.herokuapp.com/books';
 let libreria = [];
+let carrello = [];
+const colonna1 = document.getElementById('colonna1');
+const colonna2 = document.getElementById('colonna2');
+const colonna3 = document.getElementById('colonna3');
+const colonna4 = document.getElementById('colonna4');
+const colonna5 = document.getElementById('colonna5');
+
+
+addEventListener('load', init);
+
+function init() {
+    getData();
+    stampaDati();
+}
 
 const getData = async () => {
-    const data = await fetch(endpoint)
+
+    await fetch(endpoint)
         .then(response => {
-            return response.json();            
+            return response.json();
         })
         .then(data => {
-            libreria = data;            
+            libreria = data;
+            stampaDati();
         })
         .catch(err => {
             //messaggio errore            
         });
 };
 
-addEventListener('load', init);
-function init() {
-    getData();
-    stampaDati();
-}
-
-const stampaDati=()=>{
-    for (let i=0;i<libreria.length;i++){
-        
+const stampaDati = () => {
+    for (let i = 0; i < libreria.length; i += 5) {
+        colonna1.appendChild(getLibro(libreria[i]));
+        colonna2.appendChild(getLibro(libreria[i + 1]));
+        colonna3.appendChild(getLibro(libreria[i + 2]));
+        colonna4.appendChild(getLibro(libreria[i + 3]));
+        colonna5.appendChild(getLibro(libreria[i + 4]));
     }
-}
+};
+
+const getLibro = (libro) => {
+
+    // <div class="card" style="width: 18rem;">
+    //     <img src="..." class="card-img-top" alt="...">
+    //     <div class="card-body">
+    //         <h5 class="card-title">Card title</h5>
+    //         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+    //             card's content.</p>
+    //         <a href="#" class="btn btn-primary bg-danger">Compra ora</a>
+    //         <a href="#" class="btn btn-primary">Scarta</a>
+    //     </div>
+    // </div>
+
+    //card
+    let div1 = document.createElement('div');
+    div1.classList.add('card');
+    div1.setAttribute('style', 'width: 18rem; heigth: 54rem;');
+
+    //img
+    let img = document.createElement('img');
+    img.src = libro.img;
+    img.classList.add('card-img-top');
+    //img.classList.add('w-100');
+    div1.appendChild(img);
+
+    //div2
+    let div2 = document.createElement('div');
+    div2.classList.add('card-body');
+
+    //h5
+    let h5 = document.createElement('h5');
+    h5.classList.add('card-title');
+    h5.innerText = libro.title;
+    div2.appendChild(h5);
+
+
+    //pulsante1
+    let link1 = document.createElement('a');
+    link1.classList.add('btn');
+    link1.classList.add('btn-primary');
+    link1.classList.add('bg-danger');
+    link1.innerText = 'Compra ora';
+
+
+    //link1.setAttribute("onclick","aggiungiCarrello('blah');");
+    link1.setAttribute("onclick", `aggiungiCarrello(${libro.asin})`);
+
+
+    link1.href = '#';
+    div2.appendChild(link1);
+
+    //pulsante2
+    let link2 = document.createElement('a');
+    link2.classList.add('btn');
+    link2.classList.add('btn-primary');
+    link2.classList.add('bg-white');
+    link2.classList.add('text-danger');
+    link2.innerText = 'Scarta';
+    link2.href = '#';
+    div2.appendChild(link2);
+    div1.appendChild(div2);
+    return div1;
+};
+
+const aggiungiCarrello = (asin) => {
+
+    //alert('asin libro' + asin);
+    libreria.forEach((element) => {
+        if (element.asin == asin) {
+            carrello.push(element);
+        }
+    });
+};

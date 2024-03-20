bcce97fe173f03c6a890fddf0506e7c59a4de5c9
fdinocera <fdinocera@libero.it>
2024-03-20 17:10:02 +0100

    esercizio

--- Unit3/Sett1/Giorno3/epic-blog/src/app/app.component.ts
+++ Unit3/Sett1/Giorno3/epic-blog/src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+// importa interface
+import { Post } from 'src/app/models/post.interface';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'epic-blog';
+
+  record1!: Post
+  record2!: Post
+
+  constructor() {
+    this.getPosts().then((posts) => {  
+
+      var n = Math.floor(Math.random() * posts.length)
+      this.record1 = posts[n]
+
+      n = Math.floor(Math.random() * posts.length)
+      this.record2 = posts[n]
+    });
+  }
+
+  async getPosts() {
+    let response = await fetch('assets/json/db.json');
+    let data = await response.json();
+    return data;
+  }
 }

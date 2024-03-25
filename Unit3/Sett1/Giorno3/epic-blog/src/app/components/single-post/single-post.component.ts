81beec565e12e213384606739e956c152be1bf90
fdinocera <fdinocera@libero.it>
2024-03-25 17:20:26 +0100

    Esercizio Angular modifica post

--- Unit3/Sett1/Giorno3/epic-blog/src/app/components/single-post/single-post.component.ts
+++ Unit3/Sett1/Giorno3/epic-blog/src/app/components/single-post/single-post.component.ts
@@ -2,12 +2,24 @@ import { Component, Input } from '@angular/core';
 import { Post } from 'src/app/models/post.interface';
 
 @Component({
-  selector: 'app-single-post',
-  templateUrl: './single-post.component.html',
-  styleUrls: ['./single-post.component.scss']
+    selector: 'app-single-post',
+    templateUrl: './single-post.component.html',
+    styleUrls: ['./single-post.component.scss']
 })
 export class SinglePostComponent {
 
-  @Input() post!:Post
+    @Input() newPost!: Post;
+    edit: boolean = false;    
 
+    toggleEdit(): void {
+        this.edit = !this.edit;
+    }
+
+    setText() {
+        const title = <HTMLInputElement>document.getElementById('title');        
+        this.newPost.title = title.value;
+
+        const textarea = <HTMLTextAreaElement>document.getElementById('textarea');        
+        this.newPost.body = textarea.value;
+    }    
 }
